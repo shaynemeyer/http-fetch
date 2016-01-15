14d9ac0af5f90a61c195b1740f4ff43733209fef
shaynemeyer <shayne.meyer@gmail.com>
2016-01-15 10:00:31 -0800

    wired up listeners and triggers for the store

--- src/reflux/ingredients-store.jsx
+++ src/reflux/ingredients-store.jsx
@@ -5,16 +5,20 @@ var Actions = require('./actions.jsx');
 var IngredientStore = Reflux.createStore({
   listenables: [Actions],
   getIngredients: function() {
-
+    HTTP.get('/ingredients')
+    .then(function(json){
+      this.ingredients = json;
+      this.fireUpdate();
+    }.bind(this));
   },
   postIngredient: function(text) {
     // Posted ingredient to the server
     // now we got a successful callback
 
-    this.trigger('change', this.ingredients);
+
   },
   fireUpdate: function() {
-
+    this.trigger('change', this.ingredients);
   }
 });
 
